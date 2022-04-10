141ea14edd388ae7d08c659697280169e1b2e290
superestos <superestos@gmail.com>
2022-04-10 15:23:49 +0800

    token type test

--- src/syntax.h
+++ src/syntax.h
@@ -1,5 +1,82 @@
 #include <istream>
 #include <iostream>
+#include <vector>
+
+typedef enum {
+    TOKEN_ERR,
+    TOKEN_NUM,
+    TOKEN_QUOTE,
+    TOKEN_ID,
+    TOKEN_DEFINE,
+    TOKEN_LET,
+    TOKEN_LPARENT = '(',
+    TOKEN_RPARENT = ')',
+} TokenType;
+
+class Token {
+public:
+    Token(std::string name): name_{name} {
+        if (name.size() == 0) {
+            type_ = TOKEN_ERR;
+            return;
+        }
+
+        if (isNum(name[0])) {
+            for (size_t i = 1; i < name.size(); i++) {
+                if (!isNum(name[i])) {
+                    type_ = TOKEN_ERR;
+                    return;
+                }
+            }
+
+            type_ = TOKEN_NUM;
+            return;
+        }
+
+        if (name[0] == '\'') {
+            type_ = TOKEN_QUOTE;
+            return;
+        }
+
+        if (name == "define") {
+            type_ = TOKEN_DEFINE;
+            return;
+        }
+
+        if (name == "let") {
+            type_ = TOKEN_LET;
+            return;
+        }
+
+        if (name == "(") {
+            type_ = TOKEN_LPARENT;
+            return;
+        }
+
+        if (name == ")") {
+            type_ = TOKEN_RPARENT;
+            return;
+        }
+
+        type_ = TOKEN_ID;
+    }
+
+    TokenType type() {
+        return type_;
+    }
+
+    std::string& name() {
+        return name_;
+    }
+
+private:
+    TokenType type_;
+    std::string name_;
+
+    bool isNum(char c) {
+        return c >= '0' && c <= '9';
+    }
+};
 
 class TokenStream {
 public:
@@ -32,6 +109,32 @@ private:
     bool read_{true};
 
     bool is_parentheses(char c) {
-        return c == '(' || c == ')';
+        return c == TOKEN_LPARENT || c == TOKEN_RPARENT;
     }
+};
+
+class Syntax {
+public:
+    Syntax(std::istream& stream): tokenStream_{stream} {
+        read();
+    }
+
+    std::vector<Token> tokens() const {
+        return tokens_;
+    }
+
+    void read() {
+        std::string name;
+        while ((name = tokenStream_.next()) != "") {
+            tokens_.emplace_back(name);
+        }
+    }
+
+    void clear() {
+        tokens_.clear();
+    }
+
+private:
+    TokenStream tokenStream_;
+    std::vector<Token> tokens_;
 };
--- test/syntax_test.cc
+++ test/syntax_test.cc
@@ -9,6 +9,7 @@ protected:
 };
 
 TEST_F(SyntaxTest, SimpleTokenStream) {
+    iss_.clear();
     iss_ << "(+ 1 2)";
     
     EXPECT_EQ(s1_.next(), "(");
@@ -26,6 +27,7 @@ TEST_F(SyntaxTest, SimpleTokenStream) {
 }
 
 TEST_F(SyntaxTest, NestedTokenStream) {
+    iss_.clear();
     iss_ << "(let ((val (+(* 1 2)3))) (* val 5))";
     
     EXPECT_EQ(s1_.next(), "(");
@@ -51,6 +53,19 @@ TEST_F(SyntaxTest, NestedTokenStream) {
     EXPECT_EQ(s1_.next(), ")");
     EXPECT_EQ(s1_.next(), ")");
     EXPECT_EQ(s1_.next(), "");   
+}
+
+TEST(TokenTest, TokenType) {
+    ASSERT_EQ(Token("").type(), TOKEN_ERR);
+    ASSERT_EQ(Token("123").type(), TOKEN_NUM);
+    ASSERT_EQ(Token("1ab").type(), TOKEN_ERR);
+    ASSERT_EQ(Token("L1").type(), TOKEN_ID);
+    ASSERT_EQ(Token("'a").type(), TOKEN_QUOTE);
+    ASSERT_EQ(Token("define").type(), TOKEN_DEFINE);
+    ASSERT_EQ(Token("let").type(), TOKEN_LET);
+    ASSERT_EQ(Token("(").type(), TOKEN_LPARENT);
+    ASSERT_EQ(Token(")").type(), TOKEN_RPARENT);
+    ASSERT_EQ(Token("+").type(), TOKEN_ID);
 }
 
 int main(int argc, char **argv) {
