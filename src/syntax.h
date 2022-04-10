318561cc189ad53537f757c61830ebb5d8e691e5
superestos <superestos@gmail.com>
2022-04-10 13:25:14 +0800

    token divied by parentheses

--- src/syntax.h
+++ src/syntax.h
@@ -1,14 +1,39 @@
 #include <istream>
+#include <iostream>
 
-class Syntax {
+class TokenStream {
 public:
-    Syntax(std::istream& stream): stream_{stream} {}
+    TokenStream(std::istream& stream): stream_{stream} {}
     std::string& newToken() {
-        stream_ >> token_;
+        if (read_) {
+            stream_ >> buffer_;
+        }
+        read_ = true;
+
+        for (size_t i = 0; i < buffer_.size(); i++) {
+            if (buffer_[i] == '(' || buffer_[i] == ')') {
+                size_t cut = std::max(1UL, i);
+                read_ = false;
+
+                token_ = buffer_.substr(0, cut);
+                buffer_ = buffer_.substr(cut);
+                break;
+            }
+        }
+
+        if (read_) {
+            token_ = std::move(buffer_);
+        }
+        
         return token_;
     }
 
 private:
     std::istream& stream_;
+    std::string buffer_;
     std::string token_;
+    bool read_{true};
+
+    const std::string left_parentheses_{"("};
+    const std::string right_parentheses_{")"};
 };
--- test/syntax_test.cc
+++ test/syntax_test.cc
@@ -5,16 +5,27 @@
 class SyntaxTest : public ::testing::Test {
 protected:
     std::stringstream iss_;
-    Syntax s1_{iss_};
+    TokenStream s1_{iss_};
 };
 
-TEST_F(SyntaxTest, NewLineTest) {
+TEST_F(SyntaxTest, TokenStream) {
+    iss_ << "(+ 1 2)";
+    
+    EXPECT_EQ(s1_.newToken(), "(");
+    EXPECT_EQ(s1_.newToken(), "+");
+    EXPECT_EQ(s1_.newToken(), "1");
+    EXPECT_EQ(s1_.newToken(), "2");
+    EXPECT_EQ(s1_.newToken(), ")");
+    EXPECT_EQ(s1_.newToken(), "");
+
+    iss_.clear();
     iss_ << "hello world";
     EXPECT_EQ(s1_.newToken(), "hello");
     EXPECT_EQ(s1_.newToken(), "world");
+    EXPECT_EQ(s1_.newToken(), "");    
 }
 
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
