010831d2bedabf06c1ab7e5db283189c24616c26
superestos <superestos@gmail.com>
2022-04-14 18:22:48 +0800

    merge value into one type

--- include/expression/expression.h
+++ include/expression/expression.h
@@ -1,16 +1,58 @@
 #include <string>
+#include <cassert>
 
-class Value {
-    
-};
-
-class Expression {
-    virtual const Value& evaluate() = 0;
-};
+typedef enum {
+    VALUE_ERR,
+    VALUE_NUM,
+    VALUE_QUOTE,
+    VALUE_FUNCTION,
+} ValueType;
 
 typedef int Num;
 typedef std::string Quote;
 
+class Expression {
+    //virtual const Value& evaluate() = 0;
+};
+
+class Value {
+public:
+    Value(Num value): type_{VALUE_NUM}, num_{value} {}
+
+    Value(Quote value): type_{VALUE_QUOTE}, quote_{value} {}
+
+    Value(Expression value): type_{VALUE_FUNCTION}, function_{value} {}
+
+    ValueType type() const {
+        return type_;
+    }
+
+    Num num() const {
+        assert(type() == VALUE_NUM);
+        return num_;
+    }
+
+    const Quote& quote() const {
+        assert(type() == VALUE_QUOTE);
+        return quote_;
+    }
+
+    const Expression& function() const {
+        assert(type() == VALUE_FUNCTION);
+        return function_;
+    }
+
+private:
+    ValueType type_;
+    Num num_;
+    Quote quote_;
+    Expression function_;
+};
+
+
+
+
+/*
 class NumValue {
 public:
     NumValue(Num value): value_{value} {}
@@ -34,3 +76,4 @@ public:
 private:
     Quote value_;
 };
+*/
--- test/expression_test.cc
+++ test/expression_test.cc
@@ -3,9 +3,22 @@
 #include "gtest/gtest.h"
 
 TEST(valueTest, BasicValue) {
+    Value n(42);
+    Value q("hello world");
+
+    EXPECT_EQ(n.type(), VALUE_NUM);
+    EXPECT_EQ(n.num(), 42);
+
+    EXPECT_EQ(q.type(), VALUE_QUOTE);
+    EXPECT_EQ(q.quote(), "hello world");
+}
+
+/*
+TEST(valueTest, BasicValue) {
     NumValue n(42);
     QuoteValue q("hello world");
 
     EXPECT_EQ(n(), 42);
     EXPECT_EQ(q(), "hello world");
 }
+*/
