5636544a3c86fa738607d2c6511541fee81ee484
superestos <superestos@gmail.com>
2022-05-01 14:16:16 +0800

    add main evaluator

--- include/parser/parser.h
+++ include/parser/parser.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <stack>
 
 #include "parser/parse_tree.h"
---
+++ main.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+
+#include "scanner/scanner.h"
+#include "parser/parser.h"
+#include "environment/environment.h"
+
+struct Evaluator {
+    EnvironmentManager manager_{ENV_BUILTIN};
+    EnvironmentPtr env_{manager_.global()};
+
+    Scanner scanner_;
+    Parser parser_;
+
+    Evaluator(std::istream &is): scanner_{is} {}
+
+    void read() {
+        scanner_.read();
+        parser_.analyze(scanner_.tokens());
+    }
+
+    auto eval() {
+        read();
+        return Expression::create(parser_.next())->eval(env_);
+    }
+};
+
+int main(int argc, char *argv[]) {
+    std::string cursor = ">";
+
+    if (argc == 1) {
+        Evaluator evaluator(std::cin);
+        auto value = evaluator.eval();
+        std::cout << value.num() << std::endl;
+
+    } else if (argc == 2) {
+        std::string filename(argv[1]);
+        std::ifstream file;
+        file.open(filename);
+        if (!file.is_open()) {
+            std::cerr << "file " << filename << " doesn't exist!" << std::endl;
+        }
+
+        Evaluator evaluator(file);
+        auto value = evaluator.eval();
+        std::cout << value.num() << std::endl;
+
+    } else {
+        std::cout << "usage: " << std::string(argv[0]) << " [file]" << std::endl;
+    }
+
+    return 0;
+}
